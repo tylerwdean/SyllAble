506b4cd7b50c900a167c040d61a478e50e28764a
Tyler Dean <tylerwilliamdean@icloud.com>
2025-03-22 13:20:49 -0400

    started building out functionality of table paragraph component

--- client/src/Components/TableParagraph/TableParagraph.jsx
+++ client/src/Components/TableParagraph/TableParagraph.jsx
@@ -5,11 +5,48 @@ const TableParagraph = (props) => {
     const [paragraphs, setParagraphs] = useContext(FormContext);
 
     const currentParagraph = paragraphs.find((para) => para.id === props.id);
+    let rows = currentParagraph.rows;
 
     const setTitle = (newTitle) => {
         const updatedParagraph = paragraphs.map((para) => para.id === props.id ? {...para, title: newTitle} : para);
         setParagraphs(updatedParagraph);
     }
+
+    const setParagraghRows = (rows) => {
+        setParagraphs((prev) => {
+            return prev.map((para) => 
+                para.id === currentParagraph.id ? {...para, rows} : para
+            )
+        })
+    }
+
+    const addRow = () => {
+        const colNum = rows[0].length;
+        const newRow = newArray(colNum).fill("");
+        rows = [...rows, newRow];
+        setParagraphRows(rows);
+    };
+
+    const addCol = () => {
+        rows = rows.map((row) => [...row, ""]);
+        setParagraphRows(rows);
+    };
+
+    const removeRow = (rowIndex) => {
+        rows = rows.filter((row, index) => index != rowIndex);
+        setParagraphRows(rows);
+    };
+
+    const removeCol = (colIndex) => {
+        rows = rows.map((row) => row.filter((_, index) => index != colIndex));
+        setParagraghRows(rows);
+    };
+
+    const updateCell = (newData, row, col) => {
+        rows[row][col] = newData;
+        setParagraghRows(rows);
+    };
+
     return (
         <>
             <input type="text" 
@@ -17,6 +54,28 @@ const TableParagraph = (props) => {
             placeholder="Title (Optional)" 
             value={currentParagraph?.title || ""} 
             onChange={setTitle(e.target.value)}/> 
+            {rows.map((row, rowIndex) => {
+                return (
+                    <div className="row"
+                    key = {props.id + "-row-" + rowIndex}
+                    >
+                        {row.map((cell, cellIndex) => {
+                            return (
+                                <input 
+                                key = {props.id + "cell-" + rowIndex + "-" + cellIndex}
+                                className="form-control col-2"
+                                value={cell}
+                                onChange={updateCell(e.targetValue, rowIndex, cellIndex)}
+                                />
+                            )
+                        })}
+                        <button
+                        className="btn btn-danger"
+                        onClick={removeRow(rowIndex)}
+                        >-</button>
+                    </div>
+                )
+            })}
         </>
     )
 }
